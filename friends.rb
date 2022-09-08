1f9642d66a8982a12d5897d22e3b68e4b329a16a
KaraJH <karas.jones@yahoo.com>
2022-09-07 20:30:34 -0600

    Added other friend

--- friends.rb
+++ friends.rb
@@ -1,4 +1,8 @@
-best_friend = "Allie"
+best_friend = "Stephanie"
+other_friend = "Emma"
+
 if best_friend == "Allie"
   puts "This is my best friend"
+elsif other_friend == "Emma"
+  puts "This is my other friend"
 end
